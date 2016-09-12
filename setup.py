3bf6f44e418d7b22a6c0f001383babdd0ac391c2
Matt Griswold <grizz@20c.com>
2016-09-12 20:14:32 +0000

    add py3 trove classes

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
         'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet',
     ],
