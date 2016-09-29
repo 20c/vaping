30a038fba6276bfa2e351947183b9f8cbf3c01ed
Matt Griswold <grizz@20c.com>
2016-09-29 14:58:03 +0000

    revert back to standard subprocess

--- vaping/io.py
+++ vaping/io.py
@@ -1,5 +1,7 @@
 # namespace imports
-from gevent import subprocess
+import subprocess # noqa
+# FIXME
+#from gevent import subprocess
 from gevent.queue import Queue # noqa
 from gevent import Greenlet as Thread # noqa
 from gevent import joinall, sleep # noqa
