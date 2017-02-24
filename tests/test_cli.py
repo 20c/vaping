8f4ac0b12c0f83ff892e16e312cc5edbfb089850
Matt Griswold <grizz@20c.com>
2017-02-24 20:43:28 +0000

    add tests for no config startup

---
+++ tests/test_cli.py
@@ -0,0 +1,12 @@
+
+from click.testing import CliRunner
+from vaping import cli
+import pytest
+
+
+def test_start_no_home():
+    runner = CliRunner()
+    with pytest.raises(ValueError) as excinfo:
+        runner.invoke(cli.cli, ['start'], catch_exceptions=False)
+
+    assert str(excinfo.value).startswith('no config specified')
