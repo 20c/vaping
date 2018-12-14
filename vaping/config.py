98354f240bcacc0424e4b9f39b9b34e7973de0e9
Stefan Pratter <stefan@20c.com>
2018-12-14 11:10:00 +0000

    parse_interval support multi-unit strings

--- vaping/config.py
+++ vaping/config.py
@@ -9,25 +9,28 @@ def parse_interval(val):
     """
     converts a string to float of seconds
         .5 = 500ms
+        90 = 1m30s
     """
-    re_intv = re.compile(r"(?P<count>\d+)(?P<unit>\w+)")
-    match = re_intv.match(val.strip())
-    if not match:
-        raise ValueError("invalid interval string '%s'" % val)
-    unit = match.group('unit')
-    count = float(match.group('count'))
-    if unit == 's':
-        return count
-    if unit == 'm':
-        return count * 60
-    if unit == 'ms':
-        return count / 1000
-    if unit == 'h':
-        return count * 3600
-    if unit == 'd':
-        return count * 86400
+    re_intv = re.compile(r"([\d\.]+)([a-zA-Z]+)")
+    val = val.strip()
 
-    raise ValueError("unknown unit from interval string '%s'" % val)
+    total = 0.0
+    for match in re_intv.findall(val):
+        unit = match[1]
+        count = float(match[0])
+        if unit == 's':
+            total += count
+        elif unit == 'm':
+            total += count * 60
+        elif unit == 'ms':
+            total += count / 1000
+        elif unit == "h":
+            total += count * 3600
+        elif unit == 'd':
+            total += count * 86400
+        else:
+            raise ValueError("unknown unit from interval string '%s'" % val)
+    return total
 
 class Config(munge.Config):
     defaults = {
