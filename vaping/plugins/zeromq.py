f29eaf2dc3c2bcbf40a66db258466507b488aa87
Elliot Frank <elliot@20c.com>
2020-10-02 11:18:10 -0500

    Add zeromq config

--- vaping/plugins/zeromq.py
+++ vaping/plugins/zeromq.py
@@ -5,6 +5,11 @@ except ImportError:
 
 import vaping
 import vaping.plugins
+import confu.schema
+
+class ZeroMQSchema(vaping.plugins.PluginConfigSchema):
+    bind = confu.schema.Str()
+    sock = confu.schema.Str()
 
 
 @vaping.plugin.register("zeromq")
@@ -17,6 +22,8 @@ class ZeroMQ(vaping.plugins.EmitBase):
     - ctx (`zmq Context`)
     - sock (`zmq socket`)
     """
+
+    ConfigSchema = ZeroMQSchema
 
     def __init__(self, config, ctx):
         super().__init__(config, ctx)
