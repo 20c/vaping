8871d96290fa7662803b6679b94c11bfa1707064
Stefan Pratter <stefan@20c.com>
2022-03-15 11:26:17 +0000

    xxx to todo

--- src/vaping/daemon.py
+++ src/vaping/daemon.py
@@ -211,7 +211,7 @@ class Vaping:
 
             vaping.io.join_plugins(self.joins)
         except Exception as exc:
-            # XXX option to log trace
+            # TODO option to log trace
             self.log.error(exc)
             raise
 
